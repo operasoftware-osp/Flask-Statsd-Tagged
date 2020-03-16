93b73d6e531f77710e50fb4b2132c2572acfcbf3
Stanislaw Wolowski <swolowski@opera.com>
2020-03-16 14:55:16 +0100

    Added path trimming and allowed path restriction

--- flask_statsd_tagged.py
+++ flask_statsd_tagged.py
@@ -10,30 +10,26 @@ log = getLogger(__name__)
 
 
 def incr(name, value=1):
-    _add_metric_to_request('incr', name, value)
+    _add_metric_to_request("incr", name, value)
 
 
 def timing(name, value):
-    _add_metric_to_request('timing', name, value)
+    _add_metric_to_request("timing", name, value)
 
 
 def gauge(name, value):
-    _add_metric_to_request('gauge', name, value)
+    _add_metric_to_request("gauge", name, value)
 
 
 def tag(name, value):
-    if not hasattr(g, 'statsd_tags'):
+    if not hasattr(g, "statsd_tags"):
         g.statsd_tags = {}
     g.statsd_tags[name] = value
 
 
 def _add_metric_to_request(metric_type, metric_name, metric_value):
-    if not hasattr(g, 'statsd_metrics'):
-        g.statsd_metrics = {
-            'timing': [],
-            'incr': [],
-            'gauge': [],
-        }
+    if not hasattr(g, "statsd_metrics"):
+        g.statsd_metrics = {"timing": [], "incr": [], "gauge": []}
     assert metric_type in g.statsd_metrics.keys()
 
     g.statsd_metrics[metric_type].append((metric_name, metric_value))
@@ -42,8 +38,8 @@ def _add_metric_to_request(metric_type, metric_name, metric_value):
 def add_tags(metric, **tags):
     if not metric:
         return metric
-    tag_str = ','.join([('%s=%s' % (key, tags[key])) for key in sorted(tags.keys())])
-    return '%s,%s' % (metric, tag_str)
+    tag_str = ",".join([("%s=%s" % (key, tags[key])) for key in sorted(tags.keys())])
+    return "%s,%s" % (metric, tag_str)
 
 
 def _get_context_tags():
@@ -53,13 +49,29 @@ def _get_context_tags():
         return {}
 
 
-class FlaskStatsdTagged(object):
+def _trim_path(path, trim):
+    """This small function can save your InfluxDB. Since path is reported to influx as a tag, when something goes wrong or an adversary vulnerability scan happens you might report milions of paths. This will result in InfluxDB choking on cardinality caused by just one small tag. This function allows to prevent that, shortening the tag to reduce space of divverent possible values.
+    """
+    if trim:
+        return "/".join(path.split("/")[: 1 + int(trim)])
+    else:
+        return path
 
-    def __init__(self, app=None, host='localhost', port=8125, extra_tags={}):
+
+class FlaskStatsdTagged(object):
+    def __init__(
+        self,
+        app=None,
+        host="localhost",
+        port=8125,
+        extra_tags={},
+        restrict_paths_to=None,
+    ):
         self.app = app
         self.statsd_host = host
         self.statsd_port = port
         self.extra_tags = extra_tags
+        self.allowed_paths = restrict_paths_to
         if app is not None:
             self.init_app(app)
 
@@ -70,9 +82,9 @@ class FlaskStatsdTagged(object):
         self.connection = self.connect()
 
     def connect(self):
-        return StatsClient(host=self.statsd_host,
-                           port=self.statsd_port,
-                           maxudpsize=1024)
+        return StatsClient(
+            host=self.statsd_host, port=self.statsd_port, maxudpsize=1024
+        )
 
     def before_request(self):
         ctx = stack.top
@@ -85,7 +97,7 @@ class FlaskStatsdTagged(object):
 
     def after_request(self, resp):
         ctx = stack.top
-        if getattr(ctx, 'statsd_already_executed', False):
+        if getattr(ctx, "statsd_already_executed", False):
             return resp  # This would happen when teardown_request is called after after_request
         ctx.statsd_already_executed = True
 
@@ -93,8 +105,14 @@ class FlaskStatsdTagged(object):
         rusage = resource.getrusage(resource.RUSAGE_SELF)
 
         tags = dict(self.extra_tags)
-        tags.update({"path": request.path or "notfound",
-                     "status_code": resp.status_code})
+        if not request.path or (
+            self.allowed_paths and request.path not in self.allowed_paths
+        ):
+            path = "incorrect"
+        else:
+            path = request.path
+
+        tags.update({"path": path, "status_code": resp.status_code})
         tags.update(_get_context_tags())
         with self.pipeline() as pipe:
             flaskrequest = add_tags("flaskrequest", **tags)
@@ -104,19 +122,29 @@ class FlaskStatsdTagged(object):
             # NOTE: The resource-based timing will (probably) only be relevant if each flask request
             # is handled by a single process, i.e. no threads.
 
-            pipe.timing(add_tags("flask_usertime", **tags), 1000 * (rusage.ru_utime - ctx.resource_before.ru_utime))
-            pipe.timing(add_tags("flask_systime", **tags), 1000 * (rusage.ru_stime - ctx.resource_before.ru_stime))
+            pipe.timing(
+                add_tags("flask_usertime", **tags),
+                1000 * (rusage.ru_utime - ctx.resource_before.ru_utime),
+            )
+            pipe.timing(
+                add_tags("flask_systime", **tags),
+                1000 * (rusage.ru_stime - ctx.resource_before.ru_stime),
+            )
 
             if ctx.content_length != 0:
-                pipe.incr(add_tags("flask_request_datarate", **tags), ctx.content_length)
-                pipe.gauge(add_tags("flask_request_datasizegauge", **tags), ctx.content_length)
+                pipe.incr(
+                    add_tags("flask_request_datarate", **tags), ctx.content_length
+                )
+                pipe.gauge(
+                    add_tags("flask_request_datasizegauge", **tags), ctx.content_length
+                )
 
             self._send_user_metrics(pipe, tags)
         return resp
 
     @staticmethod
     def _send_user_metrics(pipe, tags):
-        metrics = getattr(g, 'statsd_metrics', {})
+        metrics = getattr(g, "statsd_metrics", {})
         for metric_type, metric_names_and_values in metrics.items():
             for metric_name, metric_value in metric_names_and_values:
                 getattr(pipe, metric_type)(add_tags(metric_name, **tags), metric_value)
@@ -125,11 +153,14 @@ class FlaskStatsdTagged(object):
         # Note that this method is expected to never fail
         class Response500(object):
             status_code = 500
+
         try:
             self.after_request(Response500())
         except:
-            log.exception("Error while tearing down request. "
-                          "This has been recovered from but statsd stuff might not have executed.")
+            log.exception(
+                "Error while tearing down request. "
+                "This has been recovered from but statsd stuff might not have executed."
+            )
 
     def pipeline(self):
         return self.connection.pipeline()
--- setup.py
+++ setup.py
@@ -2,29 +2,26 @@ from setuptools import setup
 
 
 setup(
-    name='Flask-StatsdTagged',
-    version='0.11',
-    url='https://github.com/forsberg/Flask-Statsd-Tagged/',
-    license='BSD',
-    author='Erik Forsberg',
-    author_email='forsberg@efod.se',
-    description='Flask extension for sending statsd data with tags, for use with telegraf statsd plugin',
+    name="Flask-StatsdTagged",
+    version="0.13",
+    url="https://github.com/forsberg/Flask-Statsd-Tagged/",
+    license="BSD",
+    author="Erik Forsberg",
+    author_email="forsberg@efod.se",
+    description="Flask extension for sending statsd data with tags, for use with telegraf statsd plugin",
     long_description=__doc__,
-    py_modules=['flask_statsd_tagged'],
+    py_modules=["flask_statsd_tagged"],
     zip_safe=False,
     include_package_data=True,
-    platforms='any',
-    install_requires=[
-        'Flask',
-        'statsd',
-    ],
+    platforms="any",
+    install_requires=["Flask", "statsd"],
     classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
